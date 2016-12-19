51b5dd0c7bc193ff93f83291ce4e1a84b83dcf07
Jens Melgaard <admin@it-links.dk>
2016-12-19 14:10:21 +0100

    Allowd access to the result collection for ValidatorResult

--- DotJEM.Json.Validation/JsonValidatorResult.cs
+++ DotJEM.Json.Validation/JsonValidatorResult.cs
@@ -8,15 +8,14 @@ namespace DotJEM.Json.Validation
 {
     public sealed class JsonValidatorResult 
     {
-        private readonly List<AbstractResult> results;
-
-        public bool IsValid => results.All(r => r.Value);
+        public bool IsValid => Results.All(r => r.Value);
         public bool HasErrors => !IsValid;
 
+        public IEnumerable<AbstractResult> Results { get; }
 
         public JsonValidatorResult(List<AbstractResult> results)
         {
-            this.results = results;
+            this.Results = results;
         }
         
     }
